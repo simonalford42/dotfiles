14703331d51698f05c4ea10063d292bea12541a0
Simon Connor Alford <sca63@g2-login-01.coecis.cornell.edu>
2021-12-07 23:55:59 -0500

    aliases

--- linux_zshenv.sh
+++ linux_zshenv.sh
@@ -1,7 +1,17 @@
+. ~/dotfiles/general_zshenv.sh
+
 function mvt() {
     mv "$@" ~/trash/
 }
 
 alias src='source ~/dotfiles/linux_zshenv.sh'
+export PATH=${PATH}:/share/apps/anaconda3/2021.05/bin
+alias ijob='srun --nodes=1 --ntasks-per-node=1 --time=02:00:00 --pty bash -i'
+alias gpujob='srun --nodes=1 --gpus-per-node=1 --ntasks-per-node=1 --time=02:00:00 --pty bash -i'
+alias sq='squeue -u sca63'
+# another good one is sacct
 
-. ~/dotfiles/general_zshenv.sh
+# since we don't have ag
+alias grepp='grep -rnw ./ -e' 
+
+tmux at

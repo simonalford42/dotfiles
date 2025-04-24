9220178e7b2f0ed9c6827c6ff71665575d8520cf
Simon Alford <sca63@cornell.edu>
2025-04-24 16:45:28 -0400

    update

--- setup/general_setup.sh
+++ setup/general_setup.sh
@@ -24,6 +24,7 @@ cp ../config/karabiner.json ~/.config/karabiner/assets/complex_modifications
 ln -si /Users/simon/dotfiles/mac_rc.sh /Users/simon/.bashrc
 ln -si /Users/simon/dotfiles/mac_rc.sh /Users/simon/.zshrc
 
+
 ln -si /Users/simon/dotfiles/tmux.conf /Users/simon/.tmux.conf
 ln -si /Users/simon/dotfiles/flake8 /Users/simon/.config/flake8
 ln -si /Users/simon/dotfiles/global_gitignore /Users/simon/.gitignore
